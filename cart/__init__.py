467e3633be83c642730e2fd1085d6a5bfb8e0dd2
adyabhat <adyabhat0201@gmail.com>
2025-01-28 16:06:14 +0530

    upload via git

---
+++ cart/__init__.py
@@ -0,0 +1,46 @@
+import json
+
+import products
+from cart import dao
+from products import Product
+
+
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
+
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+
+def get_cart(username):
+    cart_details = dao.get_cart(username)
+    if not cart_details:
+        return []
+
+    items = [
+        item
+        for cart_detail in cart_details
+        for item in json.loads(cart_detail["contents"])
+    ]
+
+    # Fetch and return the corresponding Product objects
+    return [get_product(item) for item in items]
+
+    
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
+
+
